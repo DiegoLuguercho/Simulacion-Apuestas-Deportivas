710170315e1732ab7a94a8292048366550156290
DiegoLuguercho <48804309+DiegoLuguercho@users.noreply.github.com>
2020-06-16 23:31:25 -0300

    Add files via upload

---
+++ src/Menú/Main.java
@@ -0,0 +1,12 @@
+package Menu;
+
+import Clases.Programa;
+import Controlador.Controlador;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		new Controlador (Programa.getInstancia()).inicio();
+	}
+}
